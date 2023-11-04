2e8c08305bf752256d783ffd3bc554bef4a8270a
leejiwon1125 <leejiwon1125@postech.ac.kr>
2023-11-04 22:36:15 +0900

    syscall basic

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -3,6 +3,7 @@
 #include <syscall-nr.h>
 #include "threads/interrupt.h"
 #include "threads/thread.h"
+#include "threads/vaddr.h"
 
 static void syscall_handler (struct intr_frame *);
 
@@ -13,9 +14,162 @@ syscall_init (void)
   lock_init(&filesys_lock);
 }
 
+static void 
+check_address (const void *addr)
+{
+  if(addr >= PHYS_BASE || addr < (void*)(0x08048000) )
+  {
+    exit(-1);
+  }
+}
+
 static void
 syscall_handler (struct intr_frame *f UNUSED) 
 {
-  printf ("system call!\n");
-  thread_exit ();
+  check_address (f->esp);
+  
+  switch (*(uint32_t *) (f->esp)) // syscall number
+  {
+    case SYS_HALT:
+    {
+      halt ();
+      break;
+    }
+    case SYS_EXIT:
+    {
+      check_address (f->esp + 4);
+
+      exit (*(int *)(f->esp + 4));
+      break;
+    }
+    case SYS_EXEC:
+    {
+      check_address (f->esp + 4);
+
+      f->eax = exec (*(const char **)(f->esp + 4));
+      break;
+    }
+    case SYS_WAIT:
+    {
+      check_address (f->esp + 4);
+
+      f->eax = wait (*(int *)(f->esp + 4)); //pid_t == int
+      break;
+    }
+    case SYS_CREATE:
+    {
+      check_address (f->esp + 4);
+      check_address (f->esp + 8);
+
+      f->eax = create (
+          *(const char **)(f->esp + 4),
+          *(unsigned *)(f->esp + 8)
+          );
+      break;
+    }
+    case SYS_REMOVE:
+    {
+      check_address (f->esp + 4);
+
+      f->eax = remove (*(const char **)(f->esp + 4));
+      break;
+    }
+    case SYS_OPEN:
+    {
+      check_address (f->esp + 4);
+
+      f->eax = open (*(const char **)(f->esp + 4));
+      break;
+    }
+    case SYS_FILESIZE:
+    {
+      check_address (f->esp + 4);
+
+      f->eax = filesize (*(int *)(f->esp + 4));
+      break;
+    }
+    case SYS_READ:
+    {
+      check_address (f->esp + 4);
+      check_address (f->esp + 8);
+      check_address (f->esp + 12);
+
+      f->eax = read (
+          *(int *)(f->esp + 4),
+          *(void **)(f->esp + 8) ,
+          *(unsigned *)(f->esp + 12)
+          );
+      break;
+    }
+    case SYS_WRITE:
+    {
+      check_address (f->esp + 4);
+      check_address (f->esp + 8);
+      check_address (f->esp + 12);
+
+      f->eax = write (
+          *(int *)(f->esp + 4),
+          *(const void **)(f->esp + 8),
+          *(unsigned *)(f->esp + 12)
+          );
+      break;
+    }
+    case SYS_SEEK:
+    {
+      check_address (f->esp + 4);
+      check_address (f->esp + 8);
+
+      seek (
+          *(int *)(f->esp + 4),
+          *(unsigned *)(f->esp + 8)
+          );
+      break;
+    }
+    case SYS_TELL:
+    {
+      check_address (f->esp + 4);
+
+      f->eax = tell (*(int *)(f->esp + 4));
+      break;
+    }
+    case SYS_CLOSE:
+    {
+      check_address (f->esp + 4);
+
+      close (*(int *)(f->esp +  4));
+      break;
+    }
+  }
 }
+
+
+void halt (void)
+{
+  shutdown_power_off ();
+}
+
+void exit (int status)
+{
+  
+}
+
+int exec (const char *file)
+{
+
+}
+
+int wait (int)
+{
+
+}
+/*
+bool create (const char *file, unsigned initial_size);
+bool remove (const char *file);
+int open (const char *file);
+int filesize (int fd);
+int read (int fd, void *buffer, unsigned length);
+int write (int fd, const void *buffer, unsigned length);
+void seek (int fd, unsigned position);
+unsigned tell (int fd);
+void close (int fd);
+*/
--- src/userprog/syscall.h
+++ src/userprog/syscall.h
@@ -4,3 +4,19 @@
 void syscall_init (void);
 
 #endif /* userprog/syscall.h */
+
+//typedef int pid_t;
+
+void halt (void);
+void exit (int status);
+int exec (const char *file);
+int wait (int);
+bool create (const char *file, unsigned initial_size);
+bool remove (const char *file);
+int open (const char *file);
+int filesize (int fd);
+int read (int fd, void *buffer, unsigned length);
+int write (int fd, const void *buffer, unsigned length);
+void seek (int fd, unsigned position);
+unsigned tell (int fd);
+void close (int fd);
